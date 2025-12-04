6dd8a9ed9a202244f0dd96bfc2838170ae0d35d2
23A91A4403 <23A91A4403@aec.edu.in>
2025-12-04 12:13:23 +0530

    Added instructor key and student keys

---
+++ generate_keys.py
@@ -0,0 +1,32 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+def generate_rsa_keypair(key_size: int = 4096):
+    # Generate private key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=key_size
+    )
+    # Generate public key
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+# Generate keys
+private_key, public_key = generate_rsa_keypair()
+
+# Save private key
+with open("student_private.pem", "wb") as f:
+    f.write(private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
+
+# Save public key
+with open("student_public.pem", "wb") as f:
+    f.write(public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    ))
+
+print("RSA key pair generated: student_private.pem and student_public.pem")
